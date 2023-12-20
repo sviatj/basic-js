bdf64fca4d1dcbf506bced3175b9260a1d4930f1
sviatj <asviatsukha@gmail.com>
2023-12-20 18:53:40 +0300

    feat: 30 pending

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,50 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+   let separator = options.separator;
+   let times = options.repeatTimes;
+   let addition = options.addition;
+   let additionRepeat = options.additionRepeatTimes;
+   let additionSeparator = options.additionSeparator;
+
+
+   if (str === null) {
+    str = 'null';
+    addition = 'null';
+    let strAddition = Array(additionRepeat).fill(addition).join(additionSeparator);
+    return Array(times).fill(str+strAddition).join(separator);
+   }
+
+   if(typeof str === 'object' && !(options.hasOwnProperty('separator'))) {
+    addition = 'STRING_OR_DEFAULT';
+    let additionFill = Array(times).fill(addition).join('')
+    return Array(times).fill(additionFill).join('+');
+   }
+
+   if(!(options.hasOwnProperty('separator'))) {
+    separator = '+';
+   }
+
+   if(!(options.hasOwnProperty('additionSeparator'))) {
+    additionSeparator = '|';
+   }
+
+   let strAddition = Array(additionRepeat).fill(addition).join(additionSeparator);
+   return Array(times).fill(str+strAddition).join(separator);
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let res = n.toString().split('').map(Number);
+  let res2 = res.reduce((a, b) => a + b, 0);
+  let res3 = res2.toString().length;
+  if (res3 > 1) {
+    let result = res2.toString().split('').map(Number);
+    let result1 = result.reduce((a, b) => a + b, 0);
+    return result1;
+  }
+  return res2;
 }
 
 module.exports = {
